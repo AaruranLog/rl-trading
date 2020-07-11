9247d26e1b639f0f8987b8d86476fed1d2e37b34
AaruranLog <aaruran.log@gmail.com>
2020-07-11 15:23:50 -0400

    reformatted

--- test_system.py
+++ test_system.py
@@ -4,12 +4,14 @@ from pandas_datareader._utils import RemoteDataError
 import numpy as np
 import math
 
+
 def test_create_env():
     env = TradingEnv()
     env.reset()
     _ = env.step(1)
-    assert len(env.prices) == len(env.data['mean'])
+    assert len(env.prices) == len(env.data["mean"])
     _ = env.data.columns
+
 
 def basic_loop(t):
     env = TradingEnv(ticker=t)
@@ -17,43 +19,50 @@ def basic_loop(t):
     done = False
     np.random.seed(885)
     while not done:
-    # for i in range(14):
+        # for i in range(14):
         action = np.random.randint(low=-1, high=2)
         next_state, r, done, _ = env.step(action)
         assert len(state) == len(next_state)
 
+
 # basic_loop_test('AAPL')
 def test_stock_name_file_exists():
     ticker_list = []
-    with open('./small_stock_name.txt') as src:
+    with open("./small_stock_name.txt") as src:
         ticker_list = src.read().split()
     assert len(ticker_list) == 82
 
+
 def test_apple_download():
-    basic_loop('AAPL')
-    
+    basic_loop("AAPL")
+
+
 def test_CELG_download_fails():
     with pytest.raises(RemoteDataError):
-        basic_loop('CELG')
-        
+        basic_loop("CELG")
+
+
 def test_action_error_float():
     env = TradingEnv()
     env.reset()
     with pytest.raises(AssertionError):
         env.step(0.99999)
-        
+
+
 def test_action_error_list():
     env = TradingEnv()
     env.reset()
     with pytest.raises(AssertionError):
         env.step([0])
-        
+
+
 def test_action_error_list():
     env = TradingEnv()
     env.reset()
     with pytest.raises(AssertionError):
-        env.step([0])        
-        
+        env.step([0])
+
+
 def test_action_succeeds():
     env = TradingEnv()
     env.reset()
@@ -63,20 +72,22 @@ def test_action_succeeds():
     _ = env.step(0)
     _ = env.step(-1)
     _ = env.step(-1)
-    
+
+
 def test_reward():
     env = TradingEnv()
     env.reset()
     _, R, __, ___ = env.step(1)
     assert isinstance(R, float)
-    
+
+
 def test_state_valid():
     env = TradingEnv()
     state = env.reset()
     for v in state:
-        assert not math.isnan(v), 'Found nan in state.'
-    
-    
+        assert not math.isnan(v), "Found nan in state."
+
+
 # def test_filtered_stocks():
 #     ticker_list = []
 #     with open('./small_stock_name.txt') as src:
