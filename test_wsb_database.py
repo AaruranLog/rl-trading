11483324c0327a12f1362ac692e7bd8bdfbb9174
AaruranLog <aaruran.log@gmail.com>
2020-07-17 20:36:27 -0400

    using fasttext to prepare sentence embeddings, off the common crawl dataset

--- test_wsb_database.py
+++ test_wsb_database.py
@@ -1,8 +1,7 @@
 import pandas as pd
 from sqlalchemy import create_engine
 import sqlite3
-from make_wsb_database import DATABASE_URI
-from filter_wsb import get_embeddings, get_all_embeddings
+from wsb_pipeline import DATABASE_URI
 
 def test_basic_query():
 	# print(pd.read_csv(file, nrows=5))
@@ -14,10 +13,10 @@ def test_basic_query():
 	queries = pd.read_sql_query('select body from wsb where created_utc = 2016-01-01 limit 1',conn)
 	# pd.read_sql_query('select * from table limit 10', csv_database)
 
-def test_wsb_db_aapl():
-	_ = get_embeddings('2016-01-01', 'AAPL')
-	_ = get_all_embeddings('AAPL')
+# def test_wsb_db_aapl():
+# 	_ = get_embeddings('2016-01-01', 'AAPL')
+# 	_ = get_all_embeddings('AAPL')
 
-def test_wsb_db_tsla():
-	_ = get_embeddings('2016-01-01', 'TSLA')
-	_ = get_all_embeddings('TSLA')
+# def test_wsb_db_tsla():
+# 	_ = get_embeddings('2016-01-01', 'TSLA')
+# 	_ = get_all_embeddings('TSLA')
---
+++ wsb_pipeline.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import pandas as pd
+import numpy as np
+import fasttext
+import fasttext.util
+from sqlalchemy import create_engine
+from tqdm import tqdm
+
+
+ft = fasttext.load_model('../../fastText/cc.en.300.bin')
+fasttext.util.reduce_model(ft, 50)
+
+def lookup_sentence_embedding(text):
+    tokens = text.split(' ')
+    word_vectors = np.array([ft.get_word_vector(t) for t in tokens])
+    return word_vectors.mean(axis=0)
+
+def create_comment_vectors(chunk):
+    
+    cleaned_chunk = pd.DataFrame({
+        'date' : pd.to_datetime(chunk['created_utc'], unit='s', origin='unix').dt.date,
+        'body' : chunk['body'],
+        'embeddings' : chunk['body'].apply(lookup_sentence_embedding)})
+    return cleaned_chunk
+
+
+# In[31]:
+
+
+chunksize=10**4
+DATABASE_URI = 'sqlite:///ft_database.db'
+if __name__ == "__main__":
+    db = create_engine(DATABASE_URI)
+
+    wsb = pd.read_json('/home/aaruran/Documents/Git/wsbData.json', lines=True, chunksize=chunksize)
+
+    for chunk in tqdm(wsb):
+        cleaned_chunk = create_comment_vectors(chunk)
+        cleaned_chunk.to_sql('wsb', db, if_exists='append', index=False)
+
