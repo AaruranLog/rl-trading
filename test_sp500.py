ba290e89fde7b165a32cb4994dd267e1c6357427
AaruranLog <aaruran.log@gmail.com>
2020-07-23 22:28:59 -0400

    tests to preserve reproducibility

--- test_sp500.py
+++ test_sp500.py
@@ -16,3 +16,14 @@ def test_blacklist():
 def test_filtered():
     with open("filtered_tickers.txt", "r") as src:
         filtered_tickers = src.read().split("\n")
+        
+
+def test_golden_blacklist():
+    blacklist = open('blacklist.txt', 'r').read().split('\n')
+    golden_blacklist = open('golden_blacklist.txt', 'r').read().split('\n')
+    assert blacklist == golden_blacklist
+    
+def test_golden_filtered():
+    filtered_tickers = open("filtered_tickers.txt", "r").read().split("\n")
+    golden_filtered_tickers = open("golden_filtered_tickers.txt", "r").read().split("\n")
+    assert golden_filtered_tickers == filtered_tickers
