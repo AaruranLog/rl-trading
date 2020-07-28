6188b8fa444394fb668943c3bd00fbc399c7919b
AaruranLog <aaruran.log@gmail.com>
2020-07-28 10:49:52 -0400

    refactored for speed

--- test_agents.py
+++ test_agents.py
@@ -21,38 +21,11 @@ def test_base_convert_action():
 
 
 def validate_net(net):
-    if (
-        torch.isnan(net.l1.weight).any()
-        or torch.isnan(net.l2.weight).any()
-        or torch.isnan(net.l3.weight).any()
-    ):
-        raise ValueError("Weight of net has become NaN")
+    assert not any(torch.isnan(p).any() for p in net.parameters())
 
 
-def test_q_net():
-    net = QNetwork()
-    x1 = FloatTensor([TradingEnv().reset()])
-    qs = net(x1)
-
-    x1[0, 0] = float("nan")
-    with pytest.raises(AssertionError):
-        net(x1)
-
-
-def test_policy_net():
-    net = PolicyNetwork()
-    x1 = FloatTensor([TradingEnv().reset()])
-    pi = net(x1, logits=False).detach()
-    assert pi.min() >= 0, f"negative probability in policy: {pi}"
-    assert abs(pi.sum() - 1) < 1e-3, f"doesn't sum to 1 {pi}"
-
-    x1[0, 0] = float("nan")
-    with pytest.raises(AssertionError):
-        net(x1)
-
-
-def basic_episode_test(agent):
-    env = TradingEnv()
+def basic_episode_test(agent, **kwargs):
+    env = TradingEnv(**kwargs)
     agent.run_episode(env)
 
 
@@ -76,74 +49,112 @@ def test_longonly_3tickers():
 
 @pytest.mark.incremental
 class TestDQN:
-    def test_dqn_basic(self):
-        basic_episode_test(DQN())
+    def test_q_net(self):
+        net = QNetwork()
+        x1 = FloatTensor([TradingEnv().reset()])
+        qs = net(x1)
 
-    def test_dqn_3tickers(self):
+        x1[0, 0] = float("nan")
+        with pytest.raises(AssertionError):
+            net(x1)
+
+    
+    def test_dqn_dev(self):
         a = DQN()
-        three_tickers_test(a)
         validate_net(a.model)
         validate_net(a.target)
-
-    def test_dqn_long(self):
-        dqn_agent = DQN()
-        dqn_agent.train(num_tickers=10, episodes_per_ticker=10, mode="train")
-
-    def test_dqn_long(self):
-        dqn_agent = DQN()
-        dqn_agent.train(num_tickers=10, episodes_per_ticker=10, mode="train")
+        basic_episode_test(a, mode="dev")
+        validate_net(a.model)
+        validate_net(a.target)
+        
+    def test_dqn_test(self):
+        a = DQN()
+        validate_net(a.model)
+        validate_net(a.target)
+        basic_episode_test(a, mode="test")
+        validate_net(a.model)
+        validate_net(a.target)        
+    
+    def test_dqn_train(self):
+        a = DQN()
+        validate_net(a.model)
+        validate_net(a.target)
+        basic_episode_test(a, mode="train")
+        validate_net(a.model)
+        validate_net(a.target)
+        
+    
 
 
 @pytest.mark.incremental
 class TestA2C:
-    def test_a2c_basic(self):
-        basic_episode_test(A2C())
+    def test_policy_net(self):
+        net = PolicyNetwork()
+        x1 = FloatTensor([TradingEnv().reset()])
+        pi = net(x1, logits=False).detach()
+        assert pi.min() >= 0, f"negative probability in policy: {pi}"
+        assert abs(pi.sum() - 1) < 1e-3, f"doesn't sum to 1 {pi}"
 
-    def test_a2c_3tickers(self):
+        x1[0, 0] = float("nan")
+        with pytest.raises(AssertionError):
+            net(x1)
+    
+    def test_a2c_dev(self):
         a = A2C()
-        three_tickers_test(a)
         validate_net(a.model)
         validate_net(a.policy)
+        basic_episode_test(a, mode='dev')
+        validate_net(a.model)
+        validate_net(a.policy)
+        
+    def test_a2c_test(self):
+        a = A2C()
+        validate_net(a.model)
+        validate_net(a.policy)
+        basic_episode_test(a, mode='test')
+        validate_net(a.model)
+        validate_net(a.policy)
+        
+    def test_a2c_train(self):
+        a = A2C()
+        validate_net(a.model)
+        validate_net(a.policy)
+        basic_episode_test(a, mode='train')
+        validate_net(a.model)
+        validate_net(a.policy)        
+        
+@pytest.mark.incremental
+class TestModelBased:
+    def test_reward_model(self):
+        m = RewardModel()
+        state, text = TradingWithRedditEnv().reset()
+        state_tensor = FloatTensor([state])
+        text_tensor  = FloatTensor(text)
+        text_tensor = text_tensor.mean(dim=0, keepdim=True)
+        m(state_tensor, text_tensor)
+        validate_net(m)
 
-    def test_a2c_large(self):
-        a2c_agent = A2C()
-        a2c_agent.train(num_tickers=10, episodes_per_ticker=1, mode="train")
-        validate_net(a2c_agent.model)
-        validate_net(a2c_agent.policy)
+    def test_q_text_model(self):
+        m = QWithTextModel()
+        state, text = TradingWithRedditEnv().reset()
+        state_tensor = FloatTensor([state])
+        text_tensor  = FloatTensor(text)
+        text_tensor = text_tensor.mean(dim=0, keepdim=True)
+        m(state_tensor, text_tensor)
+        validate_net(m)
 
-    def test_a2c_all(self):
-        pass
+    def test_transition_model(self):
+        state, text = TradingWithRedditEnv().reset()
+        state_tensor = FloatTensor([state])
+        T = TransitionModel()
+        T(state_tensor), T.next_state(state_tensor)
 
-def test_reward_model():
-    m = RewardModel()
-    state, text = TradingWithRedditEnv()
-    state_tensor = FloatTensor([state])
-    text_tensor  = FloatTensor(text)
-    text_tensor = text_tensor.mean(dim=0, keepdim=True)
-    m(state_tensor, text_tensor)
-    validate_net(m)
-    
-def test_q_text_model():
-    m = QWithTextModel()
-    state, text = TradingWithRedditEnv()
-    state_tensor = FloatTensor([state])
-    text_tensor  = FloatTensor(text)
-    text_tensor = text_tensor.mean(dim=0, keepdim=True)
-    m(state_tensor, text_tensor)
-    validate_net(m)
-    
-def test_transition_model():
-    state, text = TradingWithRedditEnv()
-    state_tensor = FloatTensor([state])
-    T = TransitionModel()
-    T(state_tensor), T.next_state(state_tensor)
-    
-def test_model_based():
-    model_agent = ModelBasedAgent()
-    e = TradingWithRedditEnv()
-    model_agent.run_episode(e)
-    
-def test_model_without_text():
-    model_without_text = ModelBased_NoText_Agent()
-    e_notext = TradingEnv()
-    model_without_text.run_episode(e_notext)
+    def test_model_based(self):
+        model_agent = ModelBasedAgent()
+        e = TradingWithRedditEnv()
+        model_agent.run_episode(e)
+
+    def test_model_without_text(self):
+        model_without_text = ModelBased_NoText_Agent()
+        e_notext = TradingEnv()
+        model_without_text.run_episode(e_notext)
